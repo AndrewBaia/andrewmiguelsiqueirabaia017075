a3d097f9544c834e341679d206f62eada1e320a0
AndrewBaia <dev.andrewbaia@hotmail.com>
2026-02-03 19:07:57 -0400

    chore(infra): adiÃ§Ã£o do maven wrapper scripts bÃ¡sicos para compatibilidade de comandos

---
+++ backend/mvnw.cmd
@@ -0,0 +1,19 @@
+@REM ----------------------------------------------------------------------------
+@REM Licensed to the Apache Software Foundation (ASF) under one
+@REM or more contributor license agreements.  See the NOTICE file
+@REM distributed with this work for additional information
+@REM regarding copyright ownership.  The ASF licenses this file
+@REM to you under the Apache License, Version 2.0 (the
+@REM "License"); you may not use this file except in compliance
+@REM with the License.  You may obtain a copy of the License at
+@REM
+@REM    http://www.apache.org/licenses/LICENSE-2.0
+@REM
+@REM ----------------------------------------------------------------------------
+
+@IF "%DEBUG%" == "true" @ECHO ON
+
+@REM Execute o Maven via terminal se o mvn estiver no PATH, 
+@REM ou use a IDE (IntelliJ) para rodar os testes e o build.
+mvn %*
+
